a8e991239124d1776737f037db63f519641009f1
Dimitri V <dvardidze15@gmail.com>
2024-07-24 15:39:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+function openFeatures() {
+    document.getElementById('features').style.display = 'block';
+    document.getElementById('fdown').style.display = 'none'
+    document.getElementById('fup').style.display = 'block'
+}
+function closeFeatures(){
+    document.getElementById('features').style.display = 'none';
+    document.getElementById('fdown').style.display = 'block'
+    document.getElementById('fup').style.display = 'none'
+}
+
+
+function openCompany() {
+    document.getElementById('company').style.display = 'block';
+    document.getElementById('cdown').style.display = 'none'
+    document.getElementById('cup').style.display = 'block'
+}
+function closeCompany() {
+    document.getElementById('company').style.display = 'none';
+    document.getElementById('cdown').style.display = 'block'
+    document.getElementById('cup').style.display = 'none'
+}
+function openMenu() {
+    document.getElementById('hiddenmenu').style.display = 'block';
+}
+function closeMenu() {
+    document.getElementById('hiddenmenu').style.display = 'none';
+}
+
+function openFeaturesMobile() {
+    document.getElementById('featurepopup').style.display = 'block'
+}
+function closeFeaturesMobile() {
+    document.getElementById('featurepopup').style.display = 'none'
+}
+
+function openCompanyMobile() {
+    document.getElementById('companypopup').style.display = 'block'
+}
+function closeCompanyMobile() {
+    document.getElementById('companypopup').style.display = 'none'
+}
